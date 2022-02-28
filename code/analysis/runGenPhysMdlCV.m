01c6480d36dbc13cf093c25ea9bf48ac26ad209c
StuartJO <sjold1@student.monash.edu>
2022-03-01 09:30:02 +1100

    Updated runGenPhysMdlCV.m
    Updated to use the inputs from the data it reads in

--- code/analysis/runGenPhysMdlCV.m
+++ code/analysis/runGenPhysMdlCV.m
@@ -1,16 +1,5 @@
 function runGenPhysMdlCV(PARC,GROWTH,MdlNum,ITER)
 
-Input.Growth = GROWTH;
-
-% Set up some default options
-Input.PD1Func = 'exponential';
-Input.IndvDist = 0;
-Input.PD2Func = 'powerlaw';
-Input.epsilon = 0;
-Input.seed = [];
-Input.normsum = 0;
-Input.TopoFunc = 'powerlaw';
-
 if PARC == 1
     mdldata = load('random200_data4physmdl.mat');
     parcname = 'random200';
@@ -33,102 +22,116 @@ else
     D = A_dist;
 end   
 
+% Set up some default options
+% Input.Growth = GROWTH;
+% Input.PD1Func = 'exponential';
+% Input.IndvDist = 0;
+% Input.PD2Func = 'powerlaw';
+% Input.epsilon = 0;
+% Input.seed = [];
+% Input.normsum = 0;
+% Input.TopoFunc = 'powerlaw';
+
 if MdlNum == 1
 
-Input.PD1Func = 'exponential';
-Input.PD2Func = 'powerlaw';
-Input.ModelNum=1;
-Input.AddMult = 'Mult';
+% Input.PD1Func = 'exponential';
+% Input.PD2Func = 'powerlaw';
+% Input.ModelNum=1;
+% Input.AddMult = 'Mult';
 PD = [];
 
 elseif MdlNum == 2
      
-Input.PD1Func = 'exponential';
-Input.PD2Func = 'powerlaw';
-Input.ModelNum=1;
-Input.AddMult = 'Add';
+% Input.PD1Func = 'exponential';
+% Input.PD2Func = 'powerlaw';
+% Input.ModelNum=1;
+% Input.AddMult = 'Add';
 
 PD = mdldata.cCGE;
 
 elseif MdlNum == 3
 
-Input.ModelNum=1;
-Input.AddMult = 'Mult';
-Input.PD1Func = 'powerlaw';
-Input.PD2Func = 'powerlaw';
+% Input.ModelNum=1;
+% Input.AddMult = 'Mult';
+% Input.PD1Func = 'powerlaw';
+% Input.PD2Func = 'powerlaw';
 
 D = mdldata.cCGE;
 PD = [];
 
 elseif MdlNum == 4
 
-Input.ModelNum=1;
-Input.AddMult = 'Add';
-Input.PD1Func = 'exponential';
-Input.PD2Func = 'powerlaw';
+% Input.ModelNum=1;
+% Input.AddMult = 'Add';
+% Input.PD1Func = 'exponential';
+% Input.PD2Func = 'powerlaw';
 
 PD = mdldata.uCGE;
 
 elseif MdlNum == 5
 
-Input.ModelNum=1;
-Input.AddMult = 'Mult';
-Input.PD1Func = 'powerlaw';
-Input.PD2Func = 'powerlaw';
+% Input.ModelNum=1;
+% Input.AddMult = 'Mult';
+% Input.PD1Func = 'powerlaw';
+% Input.PD2Func = 'powerlaw';
 
 D = mdldata.uCGE;
 PD = [];
 
 elseif MdlNum == 6
     
-Input.PD1Func = 'powerlaw';
-Input.PD2Func = 'powerlaw';
-Input.normsum = 0;
-Input.ModelNum=1;
+% Input.PD1Func = 'powerlaw';
+% Input.PD2Func = 'powerlaw';
+% Input.AddMult = 'Mult';
+% Input.ModelNum=1;
 
 D = mdldata.hist_mpc;
 PD = [];
-Input.AddMult = 'Mult';
+
 
 elseif MdlNum == 7
 
-Input.PD1Func = 'exponential';
-Input.PD2Func = 'powerlaw';
-Input.ModelNum=1;
+% Input.PD1Func = 'exponential';
+% Input.PD2Func = 'powerlaw';
+% Input.ModelNum=1;
+% Input.AddMult = 'Add';
     
 PD = mdldata.hist_mpc;
-Input.AddMult = 'Add';
 
 elseif MdlNum == 8
     
-Input.PD1Func = 'powerlaw';
-Input.PD2Func = 'powerlaw';
-Input.ModelNum=1;
+% Input.PD1Func = 'powerlaw';
+% Input.PD2Func = 'powerlaw';
+% Input.ModelNum=1;
+% Input.AddMult = 'Mult';
 
 D = mdldata.t1t2_mpc;
 PD = [];
-Input.AddMult = 'Mult';
+
 
 elseif MdlNum == 9
 
-Input.PD1Func = 'exponential';
-Input.PD2Func = 'powerlaw';
-Input.ModelNum=1;
+% Input.PD1Func = 'exponential';
+% Input.PD2Func = 'powerlaw';
+% Input.ModelNum=1;
+% Input.AddMult = 'Add';
     
 PD = mdldata.t1t2_mpc;
-Input.AddMult = 'Add';
+
 
 elseif MdlNum == 10
 
-Input.AddMult = 'Add';
-Input.ModelNum=3;
+% Input.AddMult = 'Add';
+% Input.ModelNum=3;
 PD = [];
 
 end
 
 Mdlouts = load([parcname,'_PhysMdls_Growth_',num2str(GROWTH),'_output.mat']);
 
-P = Mdlouts.MinMdlFitParams{MdlNum};
+Input = Mdlouts.Inputs{MdlNum};
+
+P = Mdlouts.OptimMdl{MdlNum}.min_maxKS.P;
 Fcv = CrossValidateModel(adjs,A_dist,D,PD,P,1,Input);
 
 Fcv.P = P;
